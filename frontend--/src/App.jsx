eba8203fc73d083cf8071f4454e1248fa95ccf52
joaovicttorbm <joaovicttorbispo@gmail.com>
2024-08-12 17:33:23 -0300

    all

--- frontend--/src/App.jsx
+++ frontend--/src/App.jsx
@@ -3,13 +3,13 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 
-
 function App() {
   return (
     <>
       <div>
         <Navbar/>
         <Hero/>
+        <p>em desenvolvimento</p>
       </div>
     </>
   )
