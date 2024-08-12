83771967bf676326163d6e3f54e1689ac865c587
joaovicttorbm <joaovicttorbispo@gmail.com>
2024-08-12 17:59:54 -0300

    ordenando

--- frontend--/src/components/Hero/HeroData.jsx
+++ frontend--/src/components/Hero/HeroData.jsx
@@ -17,13 +17,13 @@ const HeroData = [
         title: "sem fio",
         category: "fone de ouvido",
     },
-    {
-        id: 1,
-        img: Watch,
-        subtitle: "Watch",
-        title: "relógio digital",
-        category: "relógio",
-    },
+    // {
+    //     id: 1,
+    //     img: Watch,
+    //     subtitle: "Watch",
+    //     title: "relógio digital",
+    //     category: "relógio",
+    // },
     {
         id: 2,
         img: Earphone,
@@ -31,48 +31,48 @@ const HeroData = [
         title: "sem fio",
         category: "fone de ouvido",
     },
-    {
-        id: 3,
-        img: PlayStation,
-        subtitle: "PlayStation 5",
-        title: "console de jogos",
-        category: "jogos",
-    },
-    {
-        id: 4,
-        img: Smartwatch2,
-        subtitle: "Apple Watch",
-        title: "smartwatch",
-        category: "smartwatch2",
-    },
-    {
-        id: 5,
-        img: MacBook,
-        subtitle: "MacBook Pro",
-        title: "laptop",
-        category: "macbook",
-    },
-    {
-        id: 6,
-        img: JBLFlip,
-        subtitle: "JBL Flip",
-        title: "alto-falante portátil",
-        category: "alto-falante",
-    },
-    {
-        id: 7,
-        img: OculusQuest,
-        subtitle: "Oculus Quest 2",
-        title: "realidade virtual",
-        category: "vr",
-    },
-    {
-        id: 8,
-        img: Watch2,
-        subtitle: "Casio G-Shock",
-        title: "relógio digital",
-        category: "relógio",
-    }
+    // {
+    //     id: 3,
+    //     img: PlayStation,
+    //     subtitle: "PlayStation 5",
+    //     title: "console de jogos",
+    //     category: "jogos",
+    // },
+    // {
+    //     id: 4,
+    //     img: Smartwatch2,
+    //     subtitle: "Apple Watch",
+    //     title: "smartwatch",
+    //     category: "smartwatch2",
+    // },
+    // {
+    //     id: 5,
+    //     img: MacBook,
+    //     subtitle: "MacBook Pro",
+    //     title: "laptop",
+    //     category: "macbook",
+    // },
+    // {
+    //     id: 6,
+    //     img: JBLFlip,
+    //     subtitle: "JBL Flip",
+    //     title: "alto-falante portátil",
+    //     category: "alto-falante",
+    // },
+    // {
+    //     id: 7,
+    //     img: OculusQuest,
+    //     subtitle: "Oculus Quest 2",
+    //     title: "realidade virtual",
+    //     category: "vr",
+    // },
+    // {
+    //     id: 8,
+    //     img: Watch2,
+    //     subtitle: "Casio G-Shock",
+    //     title: "relógio digital",
+    //     category: "relógio",
+    // }
 ];
 
 export default HeroData;
